214e56756b86953aad1e74693205358618fd1212
Subash Chalise <subashchalise08@gmail.com>
2024-04-04 18:29:01 -0400

    updated changetextbehaviourkttest

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
@@ -74,6 +74,98 @@ class ChangeTextBehaviorKtTest {
         onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)))
     }
 
+    @Test
+    fun validateTextViewInMainActivity() {
+        // Validate correct strings in the TextView in the main activity.
+        Espresso.onView(ViewMatchers.withId(R.id.textToBeChanged))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.hello_world)))
+
+        pause()
+    }
+    @Test
+    fun validateEditTextChangeTextButton() {
+        // Enter “123” and press Change Text Button, and test the string
+        val inputText = "123"
+        Espresso.onView(ViewMatchers.withId(R.id.editTextUserInput))
+            .perform(ViewActions.typeText(inputText))
+        Espresso.onView(ViewMatchers.withId(R.id.changeTextBt))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.textToBeChanged))
+            .check(ViewAssertions.matches(ViewMatchers.withText(inputText)))
+        pause()
+    }
+    @Test
+    fun validateEditTextOpenActivityChangeTextButton() {
+        // Enter “123” and press Open Activity and Change Text Button,
+        // and test the string in ShowTextActivity
+        val inputText = "123"
+        Espresso.onView(ViewMatchers.withId(R.id.editTextUserInput))
+            .perform(ViewActions.typeText(inputText))
+        Espresso.onView(ViewMatchers.withId(R.id.activityChangeTextBtn))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.show_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText(inputText)))
+        pause()
+    }
+
+    @Test
+    fun validateEmptyEditTextChangeTextButton() {
+        // Without entering anything and press Change Text Button and test the string (empty/null)
+        Espresso.onView(ViewMatchers.withId(R.id.changeTextBt))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.textToBeChanged))
+            .check(ViewAssertions.matches(ViewMatchers.withText("")))
+        pause()
+    }
+
+    @Test
+    fun validateEmptyEditTextOpenActivityChangeTextButton() {
+        // Without entering anything and press Open Activity and Change Text Button,
+        // and test the string in ShowTextActivity (null).
+        Espresso.onView(ViewMatchers.withId(R.id.activityChangeTextBtn))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.show_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText("")))
+
+        pause()
+    }
+
+    @Test
+    fun validateNonEmptyEditTextChangeTextButton() {
+        // Enter “abcdef” and press Change Text Button, and test the string
+        val inputText = "abcdef"
+        Espresso.onView(ViewMatchers.withId(R.id.editTextUserInput))
+            .perform(ViewActions.typeText(inputText))
+        Espresso.onView(ViewMatchers.withId(R.id.changeTextBt))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.textToBeChanged))
+            .check(ViewAssertions.matches(ViewMatchers.withText(inputText)))
+
+        pause()
+    }
+
+    @Test
+    fun validateNonEmptyEditTextOpenActivityChangeTextButton() {
+        // Enter “abcdef” and press Open Activity and Change Text Button,
+        // and test the string in ShowTextActivity
+        val inputText = "abcdef"
+        Espresso.onView(ViewMatchers.withId(R.id.editTextUserInput))
+            .perform(ViewActions.typeText(inputText))
+        Espresso.onView(ViewMatchers.withId(R.id.activityChangeTextBtn))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.show_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText(inputText)))
+
+        pause()
+    }
+
+    private fun pause() {
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
     companion object {
 
         val STRING_TO_BE_TYPED = "Espresso"
